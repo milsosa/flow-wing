ec1d3d9025cfbfe7780f61dd3ad88cb2f05c44ce
Milton Sosa <sosamilton19@gmail.com>
2017-05-25 22:22:32 -0600

    Add multiple runs per flow example

--- examples/flows-as-tasks.js
+++ examples/flows-as-tasks.js
@@ -7,7 +7,7 @@ const tapLog = require('./utils/tap-log');
 const options = {
   resultsAsArray: true,
   abortOnError: false,
-  concurrency: 3
+  concurrency: 5
 };
 
 const getOptions = (opts, name) => Object.assign({}, opts, { name });
@@ -63,22 +63,47 @@ const tasks = {
   elevenToFifteen: elevenToFifteen.asTask().pipe(tapLog('elevenToFifteen'))
 };
 
+const test = [
+  oneToFive.asTask().pipe(tapLog('oneToFive1')),
+  oneToFive.asTask().pipe(tapLog('oneToFive2')),
+  oneToFive.asTask().pipe(tapLog('oneToFive3')),
+  oneToFive.asTask().pipe(tapLog('oneToFive4')),
+  oneToFive.asTask().pipe(tapLog('oneToFive5'))
+];
+
 const context = {
   some: 'data',
   delay: parseInt(process.argv[2], 10) || 100
 };
 
-flow.parallel(tasks, getOptions(options, 'mainFlow'))
-  .pipe(addTotal)
-  .run(context)
+// flow.parallel(tasks, getOptions(options, 'mainFlow'))
+//   .pipe(addTotal)
+//   .run(context)
+//   .then(data => {
+//     console.log(data);
+//     // { context: { some: 'data', delay: 100 },
+//     //   results: { total: 120 } }
+//   })
+//   .catch(err => {
+//     // err = TaskError, a VError instance
+//     console.error(VError.fullStack(err));
+//     // The error cause
+//     console.error(err.cause());
+//   });
+
+const testFlow = flow.parallel(test, getOptions(options, 'mainFlow'));
+
+for (let i = 0; i < 5; i++)  {
+  testFlow.run(context)
   .then(data => {
-    console.log(data);
+    console.log(`execution %d errors: `, i, data.errors.length);
     // { context: { some: 'data', delay: 100 },
     //   results: { total: 120 } }
   })
   .catch(err => {
     // err = TaskError, a VError instance
-    console.error(VError.fullStack(err));
+    // console.error(VError.fullStack(err));
     // The error cause
-    console.error(err.cause());
+    // console.error(err.cause());
   });
+}
