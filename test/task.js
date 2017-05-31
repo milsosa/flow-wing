4dda335471b050b50e22ef9efba03ef3f9473073
Milton Sosa <sosamilton19@gmail.com>
2017-05-30 19:07:02 -0600

    Finish unit tests implementation

---
+++ test/task.js
@@ -0,0 +1,103 @@
+import test from 'ava';
+import sinon from 'sinon';
+import Task from '../lib/flow/task';
+
+test('should expose the static .create() method', t => {
+  t.is(typeof Task.create, 'function', 'Task.create should be a function');
+});
+
+test('.create() should allow to pass task specific arguments', async t => {
+  const handler = (ctx, arg1, arg2) => arg1 + arg2;
+  const task = Task.create(handler, 5, 5);
+  const expectedResult = 10;
+
+  const actualResult = await task.run({});
+
+  t.is(actualResult, expectedResult);
+});
+
+test('.create() should assign the provided id to the task', t => {
+  const handler = ctx => ctx;
+
+  const task = Task.create('my-awesome-task', handler);
+
+  t.is(task.id, 'my-awesome-task');
+});
+
+test('.create() should use the handler/function name when id was not provided', t => {
+  function handler(ctx) {
+    return ctx;
+  }
+
+  const task = Task.create(handler);
+
+  t.is(task.id, 'handler');
+});
+
+test('.create() should throw when a non function handler is provided', t => {
+  const invalidHandler = {};
+  const expectedErrorMsg = 'The task handler should be a function, got "object"';
+
+  const error = t.throws(() => Task.create(invalidHandler), TypeError);
+
+  t.is(error.message, expectedErrorMsg);
+});
+
+test('should allow to pipe additional handlers', async t => {
+  const handler = (ctx, arg1, arg2) => arg1 + arg2;
+  const pipeHandler = (ctx, prevResult, arg1) => prevResult + arg1;
+  const task = Task.create(handler, 5, 5).pipe(pipeHandler, 5);
+  const expectedResult = 15;
+
+  const actualResult = await task.run({});
+
+  t.is(actualResult, expectedResult);
+});
+
+test('.run() should pass the provided context to its handler(s)', async t => {
+  const handler = sinon.stub().returnsArg(0);
+  const pipeHandler = sinon.stub().returnsArg(0);
+  const task = Task.create(handler).pipe(pipeHandler);
+  const context = { some: 'data' };
+  const expectedResult = context;
+
+  const actualResult = await task.run(context);
+
+  t.deepEqual(actualResult, expectedResult);
+  t.is(handler.called, true);
+  t.is(pipeHandler.called, true);
+});
+
+test('.run() should pass the provided initial value to its handler(s)', async t => {
+  const handler = sinon.stub().returnsArg(1);
+  const pipeHandler = sinon.stub().returnsArg(1);
+  const task = Task.create(handler).pipe(pipeHandler);
+  const initialValue = 'initial value';
+  const expectedResult = initialValue;
+
+  const actualResult = await task.run({}, initialValue);
+
+  t.deepEqual(actualResult, expectedResult);
+  t.is(handler.called, true);
+  t.is(pipeHandler.called, true);
+});
+
+test('.run() should reject the promise when handler(s) calls callback with error', async t => {
+  const handlerError = new Error('something went wrong');
+  const handler = sinon.stub().yields(handlerError);
+  const task = Task.create(handler);
+
+  const actualError = await t.throws(task.run({}));
+
+  t.is(actualError, handlerError);
+});
+
+test('.run() should reject the promise when handler(s) return a rejected promise', async t => {
+  const handlerError = new Error('something went wrong');
+  const handler = sinon.stub().returns(Promise.reject(handlerError));
+  const task = Task.create(handler);
+
+  const actualError = await t.throws(task.run({}));
+
+  t.is(actualError, handlerError);
+});
--- test/utils.js
+++ test/utils.js
@@ -1,11 +1,11 @@
 import test from 'ava';
 import VError from 'verror';
-import flow from '../../lib/flow';
-import Utils from '../../lib/utils';
+import flow from '../lib/flow';
+import Utils from '../lib/utils';
 
 const Task = flow.Task;
 
-test('Utils.isPromise()', t => {
+test('.isPromise() should return true when a task is passed', t => {
   const promise = Promise.resolve(true);
 
   t.true(Utils.isPromise(promise), 'should return true when passed a promise');
@@ -13,12 +13,12 @@ test('Utils.isPromise()', t => {
   t.false(Utils.isPromise({}), 'should return false when passed an object');
 });
 
-test('Utils.isFunction()', t => {
+test('.isFunction() should return true when a function is passed', t => {
   t.true(Utils.isFunction(() => {}), 'should return true for a function');
   t.false(Utils.isFunction({}), 'should return true for an object');
 });
 
-test('Utils.isTask()', t => {
+test('.isTask() should return true when a task instance is passed', t => {
   const task = Task.create(() => {});
 
   t.true(Utils.isTask(task), 'should return true for a task');
@@ -26,7 +26,7 @@ test('Utils.isTask()', t => {
   t.false(Utils.isTask({}), 'should return false for an object');
 });
 
-test('Utils.isFlow()', t => {
+test('.isFlow() should return true when a flow instance is passed', t => {
   const task = Task.create(() => {});
   const testFlow = flow([task]);
 
@@ -35,7 +35,7 @@ test('Utils.isFlow()', t => {
   t.false(Utils.isFlow({}), 'should return false for an object');
 });
 
-test('Utils.restArgs()', t => {
+test('.restArgs() should return an array of rest arguments', t => {
   const fn = function (skip) {
     return Utils.restArgs(arguments, skip);
   };
@@ -45,21 +45,21 @@ test('Utils.restArgs()', t => {
   t.is(fn(2, 'arg1', 'arg2').length, 1, 'should return an array with the rest of arguments');
 });
 
-test('Utils.buildTaskError()', t => {
+test('.buildTaskError() should build a TaskError that includes the passed cause', t => {
   const cause = new Error('something went wrong');
   const taskID = 'task-id';
   const flowOpts = { name: 'flow-name', mode: 'parallel' };
-  const taskError = Utils.buildTaskError(cause, taskID, flowOpts);
-
   const expectedInfo = {
     taskID: 'task-id',
     flowName: 'flow-name',
     flowMode: 'parallel'
   };
   const expectedMessage = [
-    `task "task-id" in flow{flow-name}:parallel has failed`,
+    'task "task-id" in flow{flow-name}:parallel has failed',
     'something went wrong'
   ].join(': ');
+
+  const taskError = Utils.buildTaskError(cause, taskID, flowOpts);
 
   t.is(taskError instanceof Error, true, 'should be an Error instance');
   t.is(taskError instanceof VError, true, 'should be a VError instance');
